d79af9564952ddf334070f25f7cd1056071f3ccd
xiansheng123 <732824600@qq.com>
2019-08-27 23:10:47 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { range } = require('rxjs');
+const { map, filter } = require('rxjs/operators');
+ 
+// range(1, 200).pipe(
+//   filter(x => x % 2 === 1),
+//   map(x => x + x)
+// ).subscribe(x => console.log(x));
+
+range(1,200).subscribe(x=>console.log(x))
