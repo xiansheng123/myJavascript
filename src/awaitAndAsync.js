61ec8a10cb0155f2b02a171a661de6790568ffd2
xiansheng123 <732824600@qq.com>
2020-01-02 23:34:01 +0800

    add promise.all test

--- src/awaitAndAsync.js
+++ src/awaitAndAsync.js
@@ -1,15 +1,38 @@
-
-
 function resolveAfter2Seconds() {
     return new Promise(resolve => {
         setTimeout(() => { resolve('resolved'); }, 2000);
     })
 }
 
-async function asyncCall(){
+async function asyncCall() {
     console.log("begin");
     let result = await resolveAfter2Seconds();
     console.log(result);
 }
 
-asyncCall()
+
+
+//asyncCall()
+const p1 = new Promise((res, reject) => {
+    setTimeout(() => {
+        console.log("this is reject begin");
+        reject(1);
+        console.log("this is reject end");
+    }, 100)
+});
+const p2 = new Promise((res, reject) => {
+    setTimeout(() => {
+        console.log("this is resolve begin");
+        res(2);
+        console.log("this is resolve end");
+    }, 3000)
+});
+
+
+// even get error response then still call all api  
+async function myPromise() {
+    await Promise.all([p1, p2]).catch(x=>console.log(x));
+    console.log("done!");
+}
+
+myPromise();
